3868c636d786e1727998a2dd646eab9dcbd00571
symbatoss <79825336+symbatoss@users.noreply.github.com>
2021-05-08 21:56:28 +0600

    2nd Home Work

--- homework_1/admin.py
+++ homework_1/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 from homework_1.models import University, Faculty, Review
+
+
+class UniversityAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
 admin.site.register(Faculty)
-admin.site.register(University)
 admin.site.register(Review)
+admin.site.register(University, UniversityAdmin)
+
